fb8170b239a5c4de5e2ec94af25aa8bc9b357037
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-01-23 20:28:39 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import okaneLogo from "./logo.png";
+
+function App() {
+  return (
+    <div className="App d-flex flex-column justify-content-center align-items-center">
+      <img src={okaneLogo} alt="Logo Okane" />
+      <div className="d-block">
+        <h1 className="App-title">Okane</h1>
+        <h2 className="App-motto">Ne dépense plus jamais de trop.</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App;
