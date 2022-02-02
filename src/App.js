b3a5b1261a285211468fd79440d1e5358147ae28
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-02-02 23:11:37 +0100

    Add github link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import okaneLogo from "./logo.png";
 function App() {
   return (
     <div className="App d-flex flex-column justify-content-center align-items-center">
-      <img src={okaneLogo} alt="Logo Okane" />
+      <a href="https://github.com/okane-app/okane"><img src={okaneLogo} alt="Logo Okane" /></a>
       <div className="d-block">
         <h1 className="App-title">Okane</h1>
         <h2 className="App-motto">Ne dépense plus jamais de trop.</h2>
